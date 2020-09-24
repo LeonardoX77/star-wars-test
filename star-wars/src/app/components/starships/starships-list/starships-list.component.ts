923dd0a671eefc07e00ab6b3ebcd45572b5e6a93
Leo <leonarx@gmail.com>
2020-09-25 01:07:05 +0200

    Creating StarShipList component

---
+++ star-wars/src/app/components/starships/starships-list/starships-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starships-list',
+  templateUrl: './starships-list.component.html',
+  styleUrls: ['./starships-list.component.scss']
+})
+export class StarShipListComponent {
+  title = 'star-wars';
+}
--- star-wars/src/app/components/starships/starships.module.ts
+++ star-wars/src/app/components/starships/starships.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
+import { StarShipListComponent } from "./starships-list/starships-list.component";
 import { StarshipsRoutes } from "./starships.routes";
 import { StarShipsService } from "./starships.service";
 
 @NgModule({
-  declarations: [ ],
+  declarations: [ StarShipListComponent ],
   imports: [
     StarshipsRoutes
   ],
