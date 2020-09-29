098f9ba61dd1226841d630a52b32d92b07a1ae8d
Leo <leonarx@gmail.com>
2020-09-29 07:34:38 +0200

    added missing CommonModule to starships module

--- star-wars/src/app/components/starships/starships.module.ts
+++ star-wars/src/app/components/starships/starships.module.ts
@@ -3,10 +3,12 @@ import { StarShipComponent } from "./starship/starship.component";
 import { StarShipListComponent } from "./starships-list/starships-list.component";
 import { StarshipsRoutes } from "./starships.routes";
 import { StarShipsService } from "./starships.service";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [ StarShipComponent, StarShipListComponent ],
   imports: [
+    CommonModule,
     StarshipsRoutes
   ],
   providers: [
